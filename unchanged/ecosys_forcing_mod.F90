49470679a6d170ef4c03bb8ee28256a5f2bc45fd
jlinmod <134327817+jlinmod@users.noreply.github.com>
2025-04-26 22:40:54 -0400

    Update ecosys_forcing_mod_docvent_removal.F90

--- unchanged/ecosys_forcing_mod.F90
+++ unchanged/ecosys_forcing_mod.F90
@@ -151,6 +151,8 @@ module ecosys_forcing_mod
   type(tracer_read)   :: iron_flux_input              ! namelist input for iron_flux
   type(tracer_read)   :: fesedflux_input              ! namelist input for fesedflux
   type(tracer_read)   :: feventflux_input             ! namelist input for feventflux
+  type(tracer_read)   :: docfluidy_input             ! namelist input for docfluidy
+
   character(char_len) :: o2_consumption_scalef_opt    ! option for specification of o2_consumption_scalef
   real(r8)            :: o2_consumption_scalef_const  ! constant for o2_consumption_scalef_opt=const
   type(tracer_read)   :: o2_consumption_scalef_input  ! file info for o2_consumption_scalef_opt=file_time_invariant
@@ -287,7 +289,8 @@ module ecosys_forcing_mod
                        potemp_ind         = 0, &
                        salinity_ind       = 0, &
                        pressure_ind       = 0, &
-                       fesedflux_ind      = 0
+                       fesedflux_ind      = 0, &
+                       docfluidy_ind      = 0
 
   !-----------------------------------------------------------------------
   ! Other private variables
@@ -380,7 +383,7 @@ contains
     namelist /ecosys_forcing_data_nml/                                        &
          dust_flux_source, dust_flux_input, iron_flux_source,                 &
          dust_ratio_thres, fe_bioavail_frac_offset, dust_ratio_to_fe_bioavail_frac_r, &
-         iron_flux_input, fesedflux_input, feventflux_input,                  &
+         iron_flux_input, fesedflux_input, feventflux_input, docfluidy_input    &
          o2_consumption_scalef_opt, o2_consumption_scalef_const,              &
          o2_consumption_scalef_input,                                         &
          p_remin_scalef_opt, p_remin_scalef_const, p_remin_scalef_input,      &
@@ -436,6 +439,7 @@ contains
     call set_defaults_tracer_read(iron_flux_input, file_varname='iron_flux')
     call set_defaults_tracer_read(fesedflux_input, file_varname='FESEDFLUXIN')
     call set_defaults_tracer_read(feventflux_input, file_varname='FESEDFLUXIN')
+    call set_defaults_tracer_read(docfluidy_input, file_varname='DOCFLUIDYR')
     o2_consumption_scalef_opt   = 'const'
     o2_consumption_scalef_const = c1
     call set_defaults_tracer_read(o2_consumption_scalef_input, file_varname='o2_consumption_scalef')
@@ -1006,6 +1010,15 @@ contains
                           filename=fesedflux_input%filename,                  &
                           file_varname=fesedflux_input%file_varname,          &
                           unit_conv_factor=fesedflux_input%scale_factor,      &
+                          rank=3, dim3_len=km, id=n)
+          case ('DOC fluid Flux')
+            fesedflux_ind = n
+            call interior_tendency_forcings(n)%add_forcing_field(                &
+                          field_source='file_time_invariant',                 &
+                          marbl_varname=marbl_varname, field_units=units,     &
+                          filename=docfluidy_input%filename,                  &
+                          file_varname=docfluidy_input%file_varname,          &
+                          unit_conv_factor=docfluidy_input%scale_factor,      &
                           rank=3, dim3_len=km, id=n)
           case ('O2 Consumption Scale Factor')
             select case (trim(o2_consumption_scalef_opt))
